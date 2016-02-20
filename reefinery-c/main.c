5ea1233439429fa73eb9b73161602c612b5ffb0f
madejean <madejean007@hotmail.fr>
2016-02-20 20:17:58 +0000

    think process

---
+++ reefinery-c/main.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+  print_square(1);
+  print_square(2);
+  print_square(3);
+  print_square(4);
+  print_square(10);
+  print_square(-2);
+  return (0);
+}
---
+++ reefinery-c/print-square.c
@@ -0,0 +1,20 @@
+#include "my_functions.h"
+void print_square(int n)
+{
+  int m, s;
+  for ( n=0; n<s; n++) {
+      {
+	for( m =0; m<s; m++ )
+	  if(n*m == 0){
+	    print_char ("o");
+
+	    
+} 
+
+
+
+
+	print_square(n);
+      }
+  }
+}
