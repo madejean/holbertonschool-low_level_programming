a3185f8c983160df682c13f3dc313a8aa6b2cccf
madejean <madejean007@hotmail.fr>
2016-03-03 15:28:40 -0800

    redefined char in prototype

--- pointers_are_fun/2-str_len.c
+++ pointers_are_fun/2-str_len.c
@@ -1,8 +1,8 @@
-int str_len(char *str)
+int str_len(char *s)
 {
   int c = 0;
 
-  while (*(str + c)) //loop to count the length of string starting from the str variable to c, the xth element of the array
+  while (*(s + c)) //loop to count the length of string starting from the str variable to c, the xth element of the array
     c++;
 
   return c;
