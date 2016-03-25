cf57bc172508102ffcb465eb4a2d5a628e56f42c
madejean <madejean007@hotmail.fr>
2016-03-25 18:06:26 +0000

    changes

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -1,6 +1,7 @@
 char *uppercase_string(char *str)
 {
   int i; 
+  /*do loop to go through the string and print uppercase letter using ascii table*/
   for (i = 0; str[i]; i++)
     {
       if (str[i] >= 97 && str[i] <= 122)
