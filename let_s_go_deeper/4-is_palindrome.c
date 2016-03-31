b89bb9b9740c760af4f63e4011f8bc3f7c40a06a
madejean <madejean007@hotmail.fr>
2016-03-31 05:00:32 +0000

    final task

--- let_s_go_deeper/4-is_palindrome.c
+++ let_s_go_deeper/4-is_palindrome.c
@@ -1,16 +1,22 @@
-int is_palindrome(char *s)
-{
-  int i;
-  int l;
+int check_palindrome(char *s, char *end, int i);
+int length(char *s);
 
-  
+int is_palindrome(char *s){
+  int l, temp;
+  l = length(s);
+  temp = l/2;
+  return check_palindrome(s, s+l-1, temp);
+}
+int check_palindrome(char *s, char *end, int i){
+  if (i == 0) 
+    return check_palindrome(s+1, end-1, i-1);
+  if (*s == *end) 
+    return 1; 
+  else return 0;
 }
 
-int str_len(char *str, int i)
-{
-  if (*(str+i) == 0)
-    {
-      return i;
-    }
-  return str_len(str, i+1);
+int length(char *s){
+  if (*s == '\0')
+    return 0;
+  return 1 + length(s+1);
 }
