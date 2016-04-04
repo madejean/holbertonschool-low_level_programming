2b37173576ab7fa0fc444b61b5e782dbde2b91af
madejean <madejean007@hotmail.fr>
2016-04-04 03:18:23 +0000

    finished task

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -2,11 +2,8 @@
 
 int main(int ac, char **av)
 {
-  int i;
- 
-  for(i = 1; i < ac; i++); 
-   printf("%s ", av[i]);
- printf("\n");
-
+  for (ac = 0; av[ac]; ac++);
+  printf("%d", ac -1);
+  printf("\n");
   return 0;
 } 
--- neither_irony_or_sarcasm_is_argument/1-args.c
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -5,7 +5,7 @@ int main(int ac, char **av)
   int i;
  
   for(i = 1; i < ac; i++) { 
-   printf("%s ", av[i]);
+   printf("%s", av[i]);
    printf("\n");
   }
   return 0;
